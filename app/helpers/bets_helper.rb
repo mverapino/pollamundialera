1fd22f3c3e81ff1de2c91d90a4ab661b39ad8852
Mauricio Vera <mverapino@gmail.com>
2014-06-17 21:03:07 -0400

    mejoras visuales a vista de todas las apuestas

--- app/helpers/bets_helper.rb
+++ app/helpers/bets_helper.rb
@@ -1,2 +1,13 @@
 module BetsHelper
+  def bet_color(bet)
+    if bet.nil?
+      ''
+    elsif bet.points ==3
+      'full-points'
+    elsif bet.points==1
+      'mid-points'
+    else
+      ''
+    end
+  end
 end
