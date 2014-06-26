4496136cbdae717292b745f659bd428fd7f82e5c
Mauricio Vera <mverapino@gmail.com>
2014-06-26 15:06:56 -0400

    fix de foreign keys en Team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ActiveRecord::Base
-  has_many :local_matches, :foreign_key => 'local', :class_name => 'Match'
-  has_many :visit_matches, :foreign_key => 'visit', :class_name => 'Match'
+  has_many :local_matches, :foreign_key => 'local_id', :class_name => 'Match'
+  has_many :visit_matches, :foreign_key => 'visit_id', :class_name => 'Match'
 
   def matches
     local_matches + visit_matches
