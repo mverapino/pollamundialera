a7535a57ac2a52cc628026be23b0c5069d855da3
Mauricio Vera <mverapino@gmail.com>
2015-06-02 20:46:49 -0300

    fix team helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -14,11 +14,13 @@ module UsersHelper
        ['Uruguay', 6],
        ['Venezuela', 12]]
     elsif type =='A'
-      [['Chile', 1], ['México', 4], ['Ecuador', 2], ['Bolivia', 3]]
+      [['Chile', 1], ['México', 2], ['Ecuador', 3], ['Bolivia', 4]]
     elsif type =='B'
-      [['Argentina', 8], ['Uruguay', 7], ['Paraguay', 5], ['Jamaica', 6]]
+      [['Argentina', 5], ['Uruguay', 6, ['Paraguay', 7], ['Jamaica', 8]]
     elsif type =='C'
-      [['Brasil', 9], ['Colombia', 11], ['Perú', 10], ['Venezuela', 12]]
+      [['Brasil', 9], ['Colombia', 10], ['Perú', 11], ['Venezuela', 12]]
+    elsif type =='Revelacion'
+      [['Bolivia', 4, ['Ecuador', 3],['Jamaica', 8], ['México', 2], ['Paraguay', 7],['Perú', 11],['Venezuela', 12]]
     else
       [['Cuarto en el grupo'],
       ['Tercero en el grupo'],
