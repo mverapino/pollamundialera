e6155851c870509a2a141a3ba43f07a6e1bb5bec
Mauricio Vera <mverapino@gmail.com>
2015-06-02 17:33:21 -0300

    allow questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def play
     #@predictions = Prediction.all.order(:id)
     #@questions = Prediction.all.order(:id).map {|a| [a, current_user.answers.where(prediction_id: a).first]}
-    @questions =[]# Prediction.all.order(:id).map {|a| [a, current_user.answers.where(prediction_id: a).first.nil? ? Answer.new(prediction:a,user:current_user) : current_user.answers.where(prediction_id: a).first]}
+    @questions = Prediction.all.order(:id).map {|a| [a, current_user.answers.where(prediction_id: a).first.nil? ? Answer.new(prediction:a,user:current_user) : current_user.answers.where(prediction_id: a).first]}
     @bets = Match.where("time > ?", Time.now ).order(:time).map {|a| [a, current_user.bets.where(match:a).first.nil? ? Bet.new(match:a, user:current_user) : current_user.bets.where(match:a).first]}
 
   end
