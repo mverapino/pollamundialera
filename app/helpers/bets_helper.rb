814e52bd586bafc7013504ab1190f1ffe8c36fe0
BuildTools <unconfigured@null.spigotmc.org>
2016-06-04 13:32:55 -0400

    fix bets helper por no mostrar fullpoints TODO: desacoplar esto

--- app/helpers/bets_helper.rb
+++ app/helpers/bets_helper.rb
@@ -2,7 +2,7 @@ module BetsHelper
   def bet_color(bet)
     if bet.nil?
       ''
-    elsif bet.points ==4
+    elsif bet.points ==4 # TODO cambiar los puntos por estados de la apuesta
       'full-points'
     elsif bet.points==2
       'mid-points'
